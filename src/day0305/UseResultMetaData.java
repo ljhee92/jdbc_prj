d01bf0088a22f1fbc87f1c282cd60a8963e34a4f
ljhee92 <weilainib7@gmail.com>
2024-03-05 22:57:34 +0900

    day0305

---
+++ src/day0305/UseResultMetaData.java
@@ -0,0 +1,13 @@
+package day0305;
+
+/**
+ * 조회하는 테이블의 Schema 정보를 얻을 때 사용하는 객체 <br>
+ * desc 테이블명 수준의 정보를 얻는다. 더 자세한 정보를 얻을 때에는 DD를 사용해야 한다.
+ */
+public class UseResultMetaData {
+
+	public static void main(String[] args) {
+
+	}	// main
+
+}	// class
